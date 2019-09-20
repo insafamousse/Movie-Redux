c5c58836995613c7f81dd96b8db989d2ee6474ff
unknown <you@example.com>
2019-09-20 14:58:18 +0200

    Updates

---
+++ precache-manifest.31509e1d31b6a67434201ef9aafe3213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb4c5d206ae9ddedfb2fa5288da6754",
+    "url": "/Movie-Redux/index.html"
+  },
+  {
+    "revision": "fc556596c7423995e2c2",
+    "url": "/Movie-Redux/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fb61fdf739c00ee0ad9c",
+    "url": "/Movie-Redux/static/js/2.e9c6bb89.chunk.js"
+  },
+  {
+    "revision": "fc556596c7423995e2c2",
+    "url": "/Movie-Redux/static/js/main.19962dcf.chunk.js"
+  },
+  {
+    "revision": "f8f301ad7f04207a8a05",
+    "url": "/Movie-Redux/static/js/runtime~main.e0072755.js"
+  },
+  {
+    "revision": "1475a81fb686c826dda69fac0c756475",
+    "url": "/Movie-Redux/static/media/Plus.1475a81f.png"
+  }
+]);
